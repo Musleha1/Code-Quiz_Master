72ef4c66a856978f5e27ca14d95199b5acf2ebb9
Musleha <86237540+Musleha1@users.noreply.github.com>
2023-01-27 16:40:57 +0000

    added clearInterval

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,6 @@
 // Define variables
 
-var highScores = document.querySelector("highscores");
+var highScores = document.querySelector("#highscores");
 var time = document.querySelector("#time");
 var startScreen = document.querySelector("#start-screen")
 var startQuizButton = document.querySelector("#start");
@@ -14,33 +14,34 @@ var submitButton = document.querySelector("#submit");
 var feedback = document.querySelector("#feedback");
 
 var timerId;
-var secondsLeft = 30;
 var questionNumber = 0;
 var questionCount = 1;
-var timeRemaining =200;
-
+var timeRemaining = 60;
+var interval;
+var correctAnswer;
 
 // Click start button to start quiz
 
 function oneSecondHandler() {
-timeRemaining --;
-time.innerText = timeRemaining; //show
+    timeRemaining --;
+    time.innerText = timeRemaining; //start countdown clock
 }
 
 function startTheQuiz() {
-    setInterval(oneSecondHandler, 1000);
+    interval = setInterval(oneSecondHandler, 1000);
     time.innerText = timeRemaining;
+    
 
-    startScreen.setAttribute("class" , "hide");
- questions.removeAttribute("class");
- renderQuestion();
+    startScreen.setAttribute("class" , "hide"); //hide welcome screen
+    questions.removeAttribute("class"); //show questions
+    renderQuestion();
 }
 
 function renderQuestion() {
-    //to be completed
-var currentQuestion = askQuestion [questionNumber]; 
-questionTitle.textContent= currentQuestion.questions;
-choices.innerHTML="";
+
+var currentQuestion = askQuestion [questionNumber]; //from question.js
+    questionTitle.textContent = currentQuestion.questions;
+    choices.innerHTML="";
 
 for(var i=0; i < currentQuestion.choices.length; i++){
     var choice = currentQuestion.choices[i];
@@ -50,9 +51,71 @@ for(var i=0; i < currentQuestion.choices.length; i++){
 
     choiceBtn.textContent = choice;
     choices.appendChild(choiceBtn);
-
+    }
 }
 
-}
 
-startQuizButton.addEventListener("click", startTheQuiz);
+function choiceHandler(event){
+    var btn = event.target;
+
+    if(!btn.matches(".choice")){
+        return;
+    }
+
+    if(btn.value !== askQuestion[questionNumber].correctAnswer){
+        timeRemaining -= 10;
+    if(time < 0) {
+        time = 0;
+        
+    }
+    
+    time.innerText = timeRemaining;
+    alert ("Wrong answer! Press OK to continue")
+    }
+    else {
+        alert ("Correct answer! Press OK to continue")    
+    }
+
+    if (timeRemaining <= 0){
+        clearInterval(interval);
+        timeRemaining.textContent = "Time is up!";
+        timeRemaining = 0
+        time.innerText = timeRemaining;
+    
+
+    }   else if(questionNumber >= askQuestion.length +1){
+        clearInterval(interval);
+
+        }
+
+    questionNumber++;
+    if (time <= 0 || questionNumber === askQuestion.length) {
+        
+        endQuiz();
+        } else {
+        renderQuestion();
+        
+      }
+      
+    }
+    startQuizButton.addEventListener("click", startTheQuiz);
+    choices.onclick = choiceHandler;
+
+function endQuiz() {
+    timeRemaining == 0 || questionNumber === askQuestion.length
+
+}
+   
+    endQuiz();
+
+    
+
+
+
+   
+
+
+
+//check if timer run out: 
+//call function endQuiz- stop timer
+//
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,33 +3,56 @@
 var askQuestion = [
     {
         questions: "Commonly used data types DO NOT include:",
-        choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"]
+        choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+
+        correctAnswer: "3. alerts"
     },
 
 
     {
         questions: "The condition of an if/ else statement is enclosed within ________.",
-        choices: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"]
+        choices: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+
+        correctAnswer: "3. parenthesis"
     },
 
     {
         questions: "String values must be enclosed within _____ when being assigned to variables.",
-        choices: ["1. commas", "2. curly brackets", "3. parenthesis", "4. quotes"]
-
+        choices: ["1. commas", "2. curly brackets", "3. parenthesis", "4. quotes"],
+        
+        correctAnswer: "4. quotes"
     },
 
     
     {
         questions: "The first index of an array is ____.",
-        choices: ["1. 0", "2. 1", "3. 2", "4. 3"]  
+        choices: ["1. 0", "2. 1", "3. 2", "4. 3"]  ,
 
+        correctAnswer: "1. 0" 
+    },
+
+    {
+        questions: "Arrays in JavaScript can be used to store ____.",
+        choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"]  ,
+
+        correctAnswer: "4. all of the above" 
+    },
+
+
+
+    {
+        questions: "How do you create a function in JavaScript?",
+        choices: ["1. function = myFunction()", "2. createMyFunction()", "3. function:myFunction()", "4. function myFunction()"],
+        
+        correctAnswer: "4. function myFunction()"
 
     },
 
     {
-        questions: "How do you create a function in JavaScript?",
-        choices: ["1. function = myFunction()", "2. createMyFunction()", "3. function:myFunction()", "4. function myFunction()"]  
+        questions: "A very useful tool used during development and debugger for printing content to the debugger is:",
+        choices: ["1. console.log", "2. terminal/ bash", "3. for loops", "4. JavaScript"]  ,
 
+        correctAnswer: "1. console.log" 
     }
 
 
