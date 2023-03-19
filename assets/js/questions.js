4c5df7d32569ddf5ba8790bd6f1b9bbeb396f709
Musleha <86237540+Musleha1@users.noreply.github.com>
2023-03-19 23:17:42 +0000

    Added question numbers

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,7 +2,7 @@
 
 var askQuestion = [
     {
-        questions: "Commonly used data types DO NOT include:",
+        questions: "1. Commonly used data types DO NOT include:",
         choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
 
         correctAnswer: "3. alerts"
@@ -10,14 +10,14 @@ var askQuestion = [
 
 
     {
-        questions: "The condition of an if/ else statement is enclosed within ________.",
+        questions: "2. The condition of an if/ else statement is enclosed within ________.",
         choices: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
 
         correctAnswer: "3. parenthesis"
     },
 
     {
-        questions: "String values must be enclosed within _____ when being assigned to variables.",
+        questions: "3. String values must be enclosed within _____ when being assigned to variables.",
         choices: ["1. commas", "2. curly brackets", "3. parenthesis", "4. quotes"],
         
         correctAnswer: "4. quotes"
@@ -25,14 +25,14 @@ var askQuestion = [
 
     
     {
-        questions: "The first index of an array is ____.",
+        questions: "4. The first index of an array is ____.",
         choices: ["1. 0", "2. 1", "3. 2", "4. 3"]  ,
 
         correctAnswer: "1. 0" 
     },
 
     {
-        questions: "Arrays in JavaScript can be used to store ____.",
+        questions: "5. Arrays in JavaScript can be used to store ____.",
         choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"]  ,
 
         correctAnswer: "4. all of the above" 
@@ -41,7 +41,7 @@ var askQuestion = [
 
 
     {
-        questions: "How do you create a function in JavaScript?",
+        questions: "6. How do you create a function in JavaScript?",
         choices: ["1. function = myFunction()", "2. createMyFunction()", "3. function:myFunction()", "4. function myFunction()"],
         
         correctAnswer: "4. function myFunction()"
@@ -49,7 +49,7 @@ var askQuestion = [
     },
 
     {
-        questions: "A very useful tool used during development and debugger for printing content to the debugger is:",
+        questions: "7. A very useful tool used during development and debugger for printing content to the debugger is:",
         choices: ["1. console.log", "2. terminal/ bash", "3. for loops", "4. JavaScript"]  ,
 
         correctAnswer: "1. console.log" 
